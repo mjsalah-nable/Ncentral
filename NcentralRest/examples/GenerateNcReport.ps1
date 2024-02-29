9716965eb64650a940be57b86decb4ddfc832b73
Mohamed Salah <mohamed.salah@n-able.com>
2024-02-29 14:56:51 -0500

    Properly construct counters to generate summary CSV report file per customer/site

--- NcentralRest/examples/GenerateNcReport.ps1
+++ NcentralRest/examples/GenerateNcReport.ps1
@@ -22,15 +22,17 @@ Connect-Ncentral -ApiHost $ApiHost -Key $jwt
 $page = 1
 $pageSize = 500
 $continue = $true
+$orgUnitMap = @{}
 
 do {
-    $orgUnits = Get-NcentralOrgUnits -Page $page -PageSize $pageSize
+    $orgUnits = Get-NcentralOrgUnits -PageNumber $page -PageSize $pageSize
     foreach ($orgUnit in $orgUnits) {
-        $orgUnitMap[$orgUnit.orgUnitId] = $orgUnit
+        $orgUnitMap[[string]$orgUnit.orgUnitId] = $orgUnit
     }
     $page++
     $continue = $orgUnits.Count -eq $pageSize
 } while ($continue)
+
 
 # Next, let's get the list of devices from the NC. devices will be aggregated based on the orgUnitId and saved to a Map collection
 # The collection will then be used to generate CSV report
@@ -39,13 +41,28 @@ $pageSize = 500
 $continue = $true
 $aggregatedReport = @{}
 
+# Create a per-orgUnitId device counter
+$orgUnitDeviceCount = @{}
+
 do {
-    $devices = Get-NcentralDevices -Page $page -PageSize $pageSize
+    $devices = Get-NcentralDevice -PageNumber $page -PageSize $pageSize
+    # Print debug info
+    # Write-Host "Page $page : $($devices.Count) devices"
     foreach ($device in $devices) {
+
+        # Print device content as JSON
+        # Write-Host "Device: $(ConvertTo-Json $device)"
+        
         $orgUnitId = $device.customerId
+        # Write-Host "Device ID: $($device.deviceId), Customer ID: $orgUnitId"
+        
         $aggregatedStruct = $aggregatedReport[$orgUnitId]
         if ($null -eq $aggregatedStruct) {
-            $BusinessUnit = $orgUnitMap[$orgUnitId]
+            $BusinessUnit = $orgUnitMap[[string]$orgUnitId]
+
+            # Print debug info
+            # Write-Host "Business Unit: $(ConvertTo-Json $BusinessUnit)"
+
 
             # Resolve so_id, so_name, customer_id, customer_name, site_id, site_name depending on $BusinessUnit's orgUnitType (SO/CUSTOMER/SITE)
             if ($BusinessUnit.orgUnitType -eq "CUSTOMER") {
@@ -58,20 +75,26 @@ do {
                 $Customer = $orgUnitMap[$BusinessUnit.parentId]
                 $SoUnit = $orgUnitMap[$Segment.parentId]
             }
+            else {
+                # None of the above, so it's a SO ... skip it
+                continue
+            }
 
+            # Print if we have reached this point
+            # Write-Host "Customer ID: $($Customer.orgUnitId), SO Name: $($SoUnit.orgUnitName), Customer Name: $($BusinessUnit.orgUnitName)"
 
             $aggregatedStruct = [PSCustomObject]@{
-                'N-Able ID'          = $Customer.orgUnitId
-                'Segment'            = $SoUnit.orgUnitName 
-                'Business Unit Name' = $BusinessUnit.orgUnitName
+                'Customer ID'           = $Customer.orgUnitId
+                'SO Name'               = $SoUnit.orgUnitName 
+                'Customer Name'         = $BusinessUnit.orgUnitName
                 #'BU Windows Assets Discovered' = $BusinessUnitDeviceCount
-                'BU Total Assets'    = 0
+                'Customer Total Assets' = 0
                 #'BU Probe Count' = Get-ProbeCount($BusinessUnit.CustomerID)
                 #'BU Discovery Started Date' =  Get-DiscoveryStartedDate($BusinessUnitProbe)
-                'Site ID'            = if ($Site) { $Site.orgUnitId } else { "" }
-                'Site Name'          = if ($Site) { $Site.orgUnitName } else { "" }
+                'Site ID'               = if ($Site) { $Site.orgUnitId } else { "" }
+                'Site Name'             = if ($Site) { $Site.orgUnitName } else { "" }
                 #'Site Windows Assets Discovered' = $SiteDeviceCount
-                'Site Total Assets'  = 0
+                'Site Total Assets'     = 0
                 #'Site Probe Count' = Get-ProbeCount($Site.CustomerID)
                 #'Site Discovery Started Date' =  Get-DiscoveryStartedDate($SiteProbe)
             }
@@ -79,14 +102,38 @@ do {
             $aggregatedReport[$orgUnitId] = $aggregatedStruct
         }
 
-        $aggregatedStruct.'BU Total Assets'++
-        if ($aggregatedReport.'Site ID') {
-            $aggregatedStruct.'Site Total Assets'++
+        # Increment the device count for the at the Business Unit level
+        $orgUnitDeviceCount[$aggregatedStruct.'Customer ID']++
+        # Print debug info, including counting the devices
+        # Write-Host "Device ID: $($device.deviceId), Customer ID: $($aggregatedStruct.'Customer ID'), Site ID: $($aggregatedStruct.'Site ID'), Customer Total Assets: $($orgUnitDeviceCount[$aggregatedStruct.'Customer ID'])"
+        
+        if ($aggregatedStruct.'Site ID') {
+            $orgUnitDeviceCount[$aggregatedStruct.'Site ID']++
         }
     }
     $page++
     $continue = $devices.Count -eq $pageSize
 } while ($continue)
+
+# Now that we have the aggregated report, loop through Map. let's update the total assets count for each orgUnitId
+foreach ($orgUnitId in $aggregatedReport.Keys) {
+    $aggregatedStruct = $aggregatedReport[$orgUnitId]
+    $aggregatedStruct.'Customer Total Assets' = $orgUnitDeviceCount[$aggregatedStruct.'Customer ID']
+    if ($aggregatedStruct.'Site ID') {
+        $aggregatedStruct.'Site Total Assets' = $orgUnitDeviceCount[$aggregatedStruct.'Site ID']
+    }
+}
+
+
+# Generate the report by converting aggregatedReport to array then exporting to a CSV file
+$report = @()
+foreach ($aggregatedStruct in $aggregatedReport.Values) {
+    Write-Host "Customer ID: $($aggregatedStruct.'Customer ID'), SO Name: $($aggregatedStruct.'SO Name'), Customer Name: $($aggregatedStruct.'Customer Name'), Customer Total Assets: $($aggregatedStruct.'Customer Total Assets'), Site ID: $($aggregatedStruct.'Site ID'), Site Name: $($aggregatedStruct.'Site Name'), Site Total Assets: $($aggregatedStruct.'Site Total Assets')"
+
+    $report += $aggregatedStruct
+}
+# export to CSV
+$report | Export-Csv -Path "NCReport.csv" -NoTypeInformation -Encoding UTF8
 
 
 # When done, disconnect from the API server
